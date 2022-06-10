abdf33f2ecf7a4dab3a1d48a29898fd2eb2e674d
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-06-10 05:52:29 +0000

    Exclude cook dir
    Signed-off-by: nιcнolaѕ wιlde <ncwilde43@gmail.com>

--- scripts/linkcheck.sh
+++ scripts/linkcheck.sh
@@ -2,6 +2,6 @@
 set -e
 set -o pipefail
 readonly DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" >/dev/null 2>&1 && pwd )
-for f in $(find "${DIR}" -not -path '*.github*' -name \*.md); do
+for f in $(find "${DIR}" -not -path '*.github*' -not -path '*cook*' -name \*.md); do
   docker run --rm -v /:/tmp:ro -i -w /tmp ghcr.io/tcort/markdown-link-check:stable "/tmp/${f}" -c "/tmp${DIR}/mlc_config.json" || exit 1
 done
